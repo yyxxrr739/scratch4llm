14153989d826d280b836657b5dc3e60732aa4e39
yyxxrr739 <yyxxrr739@163.com>
2025-06-22 13:10:29 +0800

    Add App.css for styling and update App.jsx to use new container class

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import AnimationManager from './components/AnimationManager'
+import './App.css'
 
 function App() {
   return (
-    <div className="app">
+    <div className="app-container">
       <AnimationManager />
     </div>
   )
